9f954e476419dff54ad5a4117b156c6918f8d2f4
jihuayu <8042833@qq.com>
2019-01-26 21:33:16 +0800

    更新了SharpStick的制造方法

--- src/main/java/io/jihuayu/goingages/Register/HandlesRegister.java
+++ src/main/java/io/jihuayu/goingages/Register/HandlesRegister.java
@@ -2,18 +2,16 @@ package io.jihuayu.goingages.Register;
 
 import io.jihuayu.goingages.api.GAEventHandle;
 import io.jihuayu.goingages.api.GAOreGenHandle;
-import io.jihuayu.goingages.event.EventHandle.HandleKnife;
-import io.jihuayu.goingages.event.EventHandle.HandleMob;
-import io.jihuayu.goingages.event.OreGenHandle.HandleOreGen;
-import io.jihuayu.goingages.event.EventHandle.HandlePlayer;
-
+import io.jihuayu.goingages.event.EventHandle.*;
+import io.jihuayu.goingages.event.OreGenHandle.*;
 public class HandlesRegister {
     public static final GAEventHandle items_register = new ItemsRegister();
 
 
     public static final GAEventHandle handle_knife = new HandleKnife();
     public static final GAEventHandle handle_player = new HandlePlayer();
-    public static final GAEventHandle handle_Mob = new HandleMob();
+    public static final GAEventHandle handle_mob = new HandleMob();
+    public static final GAEventHandle handle_knock = new HandleKnock();
 
 
     public static final GAOreGenHandle handle_ore_gen = new HandleOreGen();
---
+++ src/main/java/io/jihuayu/goingages/item/GrassRope.java
@@ -0,0 +1,9 @@
+package io.jihuayu.goingages.item;
+
+import io.jihuayu.goingages.api.GAItem;
+
+public class GrassRope extends GAItem {
+    public GrassRope(){
+        super("grass_rope");
+    }
+}
