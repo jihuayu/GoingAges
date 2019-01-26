c986c9c459ff57c17d62be2804e7736f9bb0d94e
jihuayu <8042833@qq.com>
2019-01-26 21:15:33 +0800

    添加了SharpStick

--- document/begin.md
+++ document/begin.md
@@ -2,3 +2,7 @@
 - 空手打草获得纤维，打树叶获得木棍
 - 敲击石头，获得尖木棍
 - 制作木小刀，割草获得纤维，割树叶获得木棍
+- GrassFiber:空手或者小刀左击草获得
+- GrassRope:3个GrassFiber无序合成
+- SharpStick：木棍敲击石头或者木头获得
+- SoulFire:Mob被烧死时获得
--- src/main/java/io/jihuayu/goingages/item/SharpStick.java
+++ src/main/java/io/jihuayu/goingages/item/SharpStick.java
@@ -3,5 +3,7 @@ package io.jihuayu.goingages.item;
 import io.jihuayu.goingages.api.GAItem;
 
 public class SharpStick extends GAItem {
-
+    public SharpStick(){
+        super("sharp_stick");
+    }
 }
