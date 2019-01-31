9092955f1f2a220d2c2ae2d16f3686c9325d1d96
jihuayu <8042833@qq.com>
2019-01-31 10:38:29 +0800

    更新了WorshipTable

--- src/main/java/io/jihuayu/goingages/Register/BlocksRegister.java
+++ src/main/java/io/jihuayu/goingages/Register/BlocksRegister.java
@@ -3,34 +3,47 @@ package io.jihuayu.goingages.Register;
 import io.jihuayu.goingages.GoingAges;
 import io.jihuayu.goingages.api.GAEventHandle;
 import io.jihuayu.goingages.block.TorchSimple;
+import io.jihuayu.goingages.block.WorshipTable;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber
 public class BlocksRegister extends GAEventHandle {
     public static Block torch_simple = new TorchSimple();
+    public static Block worship_table = new WorshipTable();
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event)
     {
         event.getRegistry().registerAll(
-                torch_simple
+                torch_simple,worship_table
         );
     }
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
         registerAll( event,
-                torch_simple
+                torch_simple,worship_table
         );
     }
 
     private static void registerAll(RegistryEvent.Register<Item> event,Block... values){
         for (Block i :values){
             event.getRegistry().register(new ItemBlock(i).setRegistryName(i.getRegistryName()));
+            registerRender(i);
         }
+    }
+    @SideOnly(Side.CLIENT)
+    private static void registerRender(Block block)
+    {
+        ModelResourceLocation model = new ModelResourceLocation(block.getRegistryName(), "inventory");
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, model);
     }
 }
--- src/main/java/io/jihuayu/goingages/block/WorshipTable.java
+++ src/main/java/io/jihuayu/goingages/block/WorshipTable.java
@@ -1,28 +1,118 @@
 package io.jihuayu.goingages.block;
 
+import io.jihuayu.goingages.GoingAges;
 import io.jihuayu.goingages.Register.ItemsRegister;
 import io.jihuayu.goingages.api.GABlock;
+import javafx.util.Pair;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityFireworkRocket;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemAir;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class WorshipTable extends GABlock {
+    private static final AxisAlignedBB AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.0625D, 1.0D);
     public WorshipTable() {
         super(Material.WOOD, "worship_table");
+        GoingAges.logger.error("yes");
     }
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
     {
         if(playerIn.getHeldItem(hand).getItem().equals(ItemsRegister.incense)){
-            this.actived(worldIn,pos,state,playerIn,hand,facing,hitX,hitY,hitZ);
+            this.activated(worldIn,pos,state,playerIn,hand,facing,hitX,hitY,hitZ);
+            worldIn.playSound((EntityPlayer)null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.BLOCK_ANVIL_BREAK, SoundCategory.PLAYERS, 0.7F, 1.0f);
             return true;
         }
         return false;
     }
-    public void actived(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ){
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        return AABB;
+    }
 
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+
+    public void activated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ){
+        if(worldIn.getBlockState(pos.add(0,0,-1)).getBlock().equals(Blocks.CHEST)){
+            TileEntity tileEntity = worldIn.getTileEntity(pos.add(0,0,-1));
+            if(tileEntity instanceof TileEntityChest){
+                Map<Item,Integer> items = new HashMap<>();
+                Set<Integer> airs = new HashSet<>();
+                TileEntityChest chest = (TileEntityChest)tileEntity;
+                for(int i =0 ;i<chest.getSizeInventory();i++){
+                    ItemStack in = chest.getStackInSlot(i);
+                    if(!in.getItem().equals(Items.AIR)){
+
+                        if(items.containsKey(in.getItem()))
+                            items.put(in.getItem(),in.getCount()+items.get(in.getItem()));
+                        else
+                            items.put(in.getItem(),in.getCount());
+                    }
+                    else{
+                        airs.add(i);
+                    }
+                }
+                if(airs.isEmpty())return;
+                for(Item i :items.keySet()){
+                    if(i.equals(Items.STICK)){
+                        int count = items.get(i)/4*4;
+                        if(count==0)break;
+                        for(int j =0 ;j<chest.getSizeInventory();j++){
+                            ItemStack in = chest.getStackInSlot(j);
+                            if(!in.getItem().equals(Items.STICK))continue;
+                            if(in.getCount()>count){
+                                chest.decrStackSize(j,count);
+                                break;
+                            }
+                            else{
+                                count-=in.getCount();
+                                chest.removeStackFromSlot(j);
+                                airs.add(j);
+                            }
+                        }
+                        count = items.get(i)/4;
+                        for(int j :airs){
+                            if(count>64){
+                                chest.setInventorySlotContents(j,new ItemStack(Blocks.LOG,64));
+                                count-=64;
+                            }
+                            else{
+                                chest.setInventorySlotContents(j,new ItemStack(Blocks.LOG,count));
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
