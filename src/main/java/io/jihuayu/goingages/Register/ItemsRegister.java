c0d5fb4004d1a6126e2e515eba0e22c351a81379
jihuayu <8042833@qq.com>
2019-01-31 15:22:54 +0800

    将小刀改成了木小刀

--- src/main/java/io/jihuayu/goingages/Register/ItemsRegister.java
+++ src/main/java/io/jihuayu/goingages/Register/ItemsRegister.java
@@ -8,13 +8,10 @@ import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.registries.ForgeRegistry;
-
-import java.util.Iterator;
 
 @Mod.EventBusSubscriber
 public class ItemsRegister extends GAEventHandle {
-    public static final Item knife_diamond =  new Knife(Item.ToolMaterial.DIAMOND);
+    public static final Item wood_knife =  new WoodKnife();
     public static final Item grass_fiber = new GrassFiber();
     public static final Item soul_fire = new SoulFire();
     public static final Item sharp_stick = new SharpStick();
@@ -24,7 +21,7 @@ public class ItemsRegister extends GAEventHandle {
     {
 //        ((ForgeRegistry<Item>)event.getRegistry()).remove(Items.COAL.getRegistryName());
         event.getRegistry().registerAll(
-                knife_diamond,grass_fiber,soul_fire,sharp_stick,incense
+                wood_knife,grass_fiber,soul_fire,sharp_stick,incense
         );
     }
     @SubscribeEvent
--- src/main/java/io/jihuayu/goingages/item/tool/WoodKnife.java
+++ src/main/java/io/jihuayu/goingages/item/tool/WoodKnife.java
@@ -2,6 +2,7 @@ package io.jihuayu.goingages.item.tool;
 
 import com.google.common.collect.Sets;
 import io.jihuayu.goingages.api.GAMaterialTool;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
@@ -13,10 +14,10 @@ import javax.annotation.Nullable;
 import java.util.List;
 
 
-public class Knife extends GAMaterialTool {
+public class WoodKnife extends GAMaterialTool {
 
-    public Knife(ToolMaterial materialIn) {
-        super("knife",materialIn, Sets.newHashSet(Blocks.LEAVES,Blocks.LEAVES2,Blocks.TALLGRASS,Blocks.DOUBLE_PLANT));
+    public WoodKnife() {
+        super("wood_knife",ToolMaterial.WOOD, Sets.newHashSet(Blocks.LEAVES,Blocks.LEAVES2,Blocks.TALLGRASS,Blocks.DOUBLE_PLANT));
     }
     @Override
     public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn)
