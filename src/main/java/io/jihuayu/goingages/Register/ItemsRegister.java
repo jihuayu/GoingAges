5c002488da001292f26b1ffc33d41379a09703d8
jihuayu <8042833@qq.com>
2019-01-31 15:27:31 +0800

    更新材质注册

--- src/main/java/io/jihuayu/goingages/Register/ItemsRegister.java
+++ src/main/java/io/jihuayu/goingages/Register/ItemsRegister.java
@@ -3,11 +3,17 @@ package io.jihuayu.goingages.Register;
 import io.jihuayu.goingages.api.GAEventHandle;
 import io.jihuayu.goingages.item.*;
 import io.jihuayu.goingages.item.tool.*;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber
 public class ItemsRegister extends GAEventHandle {
@@ -16,18 +22,32 @@ public class ItemsRegister extends GAEventHandle {
     public static final Item soul_fire = new SoulFire();
     public static final Item sharp_stick = new SharpStick();
     public static final Item  incense = new Incense();
+    public static final Item grass_rope  =new GrassRope();
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
-//        ((ForgeRegistry<Item>)event.getRegistry()).remove(Items.COAL.getRegistryName());
-        event.getRegistry().registerAll(
-                wood_knife,grass_fiber,soul_fire,sharp_stick,incense
+        registerAll(event,
+                wood_knife,grass_fiber,soul_fire,sharp_stick,incense,grass_rope
         );
     }
+    private static void registerAll(RegistryEvent.Register<Item> event,Item... values){
+        for (Item i :values){
+            event.getRegistry().register(i.setRegistryName(i.getRegistryName()));
+            registerRender(i);
+        }
+    }
+    @SideOnly(Side.CLIENT)
+    private static void registerRender(Item item)
+    {
+        ModelResourceLocation model = new ModelResourceLocation(item.getRegistryName(), "inventory");
+        ModelLoader.setCustomModelResourceLocation(item, 0, model);
+    }
+
     @SubscribeEvent
     public static void registerRecipes(RegistryEvent.Register<IRecipe> event)
     {
         //((ForgeRegistry<IRecipe>)event.getRegistry()).clear();
 
     }
+
 }
